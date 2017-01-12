2e9560d458ab4dfbddc47b2209ff9c57fb92817e
Mathias Balogh <mathiasbalogh@Mathias-MacBook-Air.local>
2017-01-12 15:45:26 -0600

    base functionality complete

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var applePrice = 2.00 , orangePrice = 2.00 , bananaPrice = 2.00 , grapePrice = 2.00 , time = 100 , currentMoney = 100.00;
+var applePrice = 2.00, orangePrice = 2.00, bananaPrice = 2.00, grapePrice = 2.00, time = 10, currentMoney = 100.00, appleCounter = 0, orangeCounter = 0, bananaCounter = 0, grapeCounter = 0;
 alert('Welcome to the fruit bazaar! How much profit can you generate in your time here? Click OK to play!');
 var timeDisplay = setInterval(function(){
 			time--;
@@ -46,43 +46,81 @@ var priceUpdate = setInterval(function(){
 
 $(function(){
 
-
 	$('#currentApplePrice').text(applePrice.toFixed(2));
 	$('#currentOrangePrice').text(orangePrice.toFixed(2));
 	$('#currentBananaPrice').text( bananaPrice.toFixed(2));
 	$('#currentGrapePrice').text(grapePrice.toFixed(2));
 	$('#money').text(currentMoney.toFixed(2));
 
-	$('.buy').on('click', function() {
-		var fruitValue=Number($(this).parent().parent().parent().find('.currentInventory').text());
-		var change = $(this).parent().parent().parent().find('.currentPrice').text();
-		change = change.substring(1, change.length);
-		if (currentMoney >= change) {
-			currentMoney -= change;
-			console.log($(this).parent().parent().parent().find('.currentPrice').text());
-			$('#money').text(currentMoney.toFixed(2));
-			fruitValue++;
-			$(this).parent().parent().parent().find('.currentInventory').text(fruitValue);
-		}
-	});
 
-	$('.sell').on('click', function() {
-		var fruitValue=Number($(this).parent().parent().parent().find('.currentInventory').text());
-		// fruitValue--;
-		if (fruitValue > 0) {
-			fruitValue--;
-			$(this).parent().parent().parent().find('.currentInventory').text(fruitValue);
-			var change = $(this).parent().parent().parent().find('.currentPrice').text();
-			change = change.substring(1, change.length);
-			currentMoney += Number(change);
-			$('#money').text(currentMoney.toFixed(2));
-		} else {
-			fruitValue = 0;
-			$(this).parent().parent().parent().find('.currentInventory').text(fruitValue);
-		}
+		$('.buy').on('click', function() {
+			if (time > 0) {
+				var fruitValue=Number($(this).parent().parent().parent().find('.currentInventory').text());
+				var change = $(this).parent().parent().parent().find('.currentPrice').text();
+				change = change.substring(1, change.length);
+				if (currentMoney >= change) {
+					currentMoney -= change;
+					// console.log($(this).parent().parent().parent().find('.currentPrice').text());
+					$('#money').text(currentMoney.toFixed(2));
+					fruitValue++;
+					$(this).parent().parent().parent().find('.currentInventory').text(fruitValue);
+					var average=Number($(this).parent().parent().parent().find('.averagePrice').text());
+					switch($(this).parent().parent().parent().find('.fruitName').text()){
+						case 'Apple':
+						appleCounter++;
+						if (appleCounter == 1) {
+							average = change;
+						} else {
+							average = ((average * (appleCounter-1))+Number(change))/appleCounter;
+						}
+						break;
+						case 'Orange':
+						orangeCounter++;
+						if (orangeCounter == 1) {
+							average = change;
+						} else {
+							average = ((average * (orangeCounter-1))+Number(change))/orangeCounter;
+						}
+						break;
+						case 'Banana':
+						bananaCounter++;
+						if (bananaCounter == 1) {
+							average = change;
+						} else {
+							average = ((average * (bananaCounter-1))+Number(change))/bananaCounter;
+						}
+						break;
+						default:
+						grapeCounter++;
+						if (grapeCounter == 1) {
+							average = change;
+						} else {
+							average = ((average * (grapeCounter-1))+Number(change))/grapeCounter;
+						}
+					}
+					average=Number(average);
+					$(this).parent().parent().parent().find('.averagePrice').text(average.toFixed(2));
+				}
+			}
+		});
 
-	});
-
+		$('.sell').on('click', function() {
+			if (time > 0) {
+				var fruitValue=Number($(this).parent().parent().parent().find('.currentInventory').text());
+				// fruitValue--;
+				if (fruitValue > 0) {
+					fruitValue--;
+					$(this).parent().parent().parent().find('.currentInventory').text(fruitValue);
+					var change = $(this).parent().parent().parent().find('.currentPrice').text();
+					change = change.substring(1, change.length);
+					currentMoney += Number(change);
+					$('#money').text(currentMoney.toFixed(2));
+				} else {
+					fruitValue = 0;
+					$(this).parent().parent().parent().find('.currentInventory').text(fruitValue);
+				}
+			}
+		});
 });
 
 function randomNumber(min, max) {
